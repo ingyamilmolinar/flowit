789d3a45b5bc9c4213cb910aed0cbf9bf9b4f1d2
Yamil Rivera <yamil.rivera@wizeline.com>
2020-03-28 12:51:51 -0400

    Writting blackbox config processing tests

--- internal/configs/main_test.go
+++ internal/configs/main_test.go
@@ -18,6 +18,10 @@ var _ = Describe("Configs", func() {
 				flowit := (*flowitptr)
 				Expect(err).To(BeNil())
 				Expect(*flowit.Version).To(Equal("0.1"))
+				Expect(*flowit.Config.Shell).To(Equal("/usr/bin/env bash"))
+				Expect((*flowit.Variables)["gerrit-port"]).To(Equal(29418))
+				Expect(*flowit.Workflow.Branches[0].ID).To(Equal("master"))
+				Expect((*flowit.Workflow.Stages[1])["actions"].([]interface{})[0].(string)).To(Equal("git checkout master"))
 			})
 
 		})
